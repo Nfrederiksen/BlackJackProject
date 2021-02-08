99d1a9f6a910af9e35555a68a287959f55d7e505
Nichollas <nicfr426@student.liu.se>
2021-02-08 12:00:54 +0100

    Changed one thing

--- BlackJackGame/Program.cs
+++ BlackJackGame/Program.cs
@@ -6,7 +6,7 @@ namespace BlackJackGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is going to be my blackjack game!");
         }
     }
 }
